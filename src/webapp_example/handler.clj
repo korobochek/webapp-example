6d58466ad16372c111e37119bc413db5de9469ee
Olga Goloshchapova <olga.goloshchapova@rea-group.com>
2016-02-21 07:24:07 +1100

    Adding post request

--- src/webapp_example/handler.clj
+++ src/webapp_example/handler.clj
@@ -1,11 +1,35 @@
 (ns webapp-example.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [webapp-example.models.item :as item]
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
+            [clojure.data.json :refer [write-str]]
+            [ring.util.response :refer [response status]]))
+
+(def item-routes
+  (context "/items" []
+    (POST "/" request (do (item/save! (:body request))
+                          (-> (response {})
+                              (status 201))))
+    (GET "/" [] (response (item/all)))
+    (GET "/:item-id" [item-id] (response (item/with-id item-id)))))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  item-routes
   (route/not-found "Not Found"))
 
+(defn wrap-exceptions
+  [handler]
+  (fn [request & opts]
+    (try
+      (handler request)
+      (catch Exception e
+        (-> (response (write-str {:message (.getMessage e)})) (status 500))))))
+
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+      (wrap-defaults (assoc-in site-defaults [:security :anti-forgery] false))
+      (wrap-json-body {:keywords? true})
+      wrap-json-response
+      wrap-exceptions))
--- test/webapp_example/handler_test.clj
+++ test/webapp_example/handler_test.clj
@@ -1,13 +1,41 @@
 (ns webapp-example.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
+            [clojure.data.json :as json]
             [webapp-example.handler :refer :all]))
 
+(def items-response [{:name "get all - 1"  :quantity 10},
+                     {:name "get all - 2" :quantity 12}])
+(def items-by-id-response [{:name "git by id"  :quantity 10}])
+(def post-item {:name "posted item" :quantity 20})
+
 (deftest test-app
-  (testing "main route"
-    (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+  (with-redefs [webapp-example.models.item/all (fn [] items-response)
+                webapp-example.models.item/with-id (fn [_] items-by-id-response)
+                webapp-example.models.item/save! (fn [_] "")]
+    (testing "get /items"
+      (let [response (app (mock/request :get "/items"))]
+        (testing "should return status 200"
+          (is (= (:status response) 200)))
+        (testing "should return items"
+          (is (= (:body response) (json/write-str items-response))))))
+    (testing "get /items/:item-id"
+      (let [response (app (mock/request :get (str "/items/5")))]
+        (testing "should return status 200"
+          (is (= (:status response) 200)))
+        (testing "should return an item by id"
+          (is (= (:body response) (json/write-str items-by-id-response))))))
+    (testing "post to /items with application/json content type"
+      (let [response (app (mock/content-type (mock/request :post "/items" (json/write-str post-item)) "application/json"))]
+        (testing "should return status 201"
+          (is (= (:status response) 201))))))
+
+  (testing "exception handling"
+    (with-redefs [webapp-example.models.item/all (fn [] (throw (Exception. "error message")))]
+      (let [response (app (mock/request :get "/items") "application/json")]
+        (testing "should return status 500"
+          (is (= (:status response) 500))
+          (is (= (:body response) (json/write-str {:message "error message"})))))))
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
