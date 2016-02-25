6ffc8515aa8367f779ecc3d38d10b09d1923b887
Olga Goloshchapova <olga.goloshchapova@rea-group.com>
2016-02-25 20:38:07 +1100

    adding project descr and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject webapp-example "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "simple webapp"
+  :url "http://simple-webapp.com/"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
